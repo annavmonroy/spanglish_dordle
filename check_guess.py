5ecc643e828941942caacac87841c280b9957148
annavmonroy <valades.a@northeastern.edu>
2022-11-04 17:20:07 -0400

    final random words

---
+++ check_guess.py
@@ -0,0 +1,11 @@
+# only checks if guess is a valid input
+class Check_Guess:
+
+    def __init_(self):
+        pass
+
+    def length(self):   # length = 5
+        pass
+
+    def letters(self):  # no symbols, accent marks, tildes, spaces, etc. # convert everything to uppercase
+        pass
