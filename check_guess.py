1640d118934412e7e06d00a6f76aabec6f0bc63e
annavmonroy <valades.a@northeastern.edu>
2022-11-05 10:34:03 -0400

    check guess try 1

--- check_guess.py
+++ check_guess.py
@@ -1,11 +1,26 @@
 # only checks if guess is a valid input
 class Check_Guess:
 
-    def __init_(self):
-        pass
+    def __init_(self, guess, order, lowercase, uppercase):
+        self.guess = guess
+        self.order = order
+        self.lowercase = lowercase
+        self.uppercase = uppercase
 
     def length(self):   # length = 5
-        pass
+        if len(self.guess) != 5:
+            raise ValueError('Invalid guess')
 
     def letters(self):  # no symbols, accent marks, tildes, spaces, etc. # convert everything to uppercase
-        pass
+        for x in range(0, len(self.guess)):
+            # converts the value in the string to it's unicode code 
+            self.order = ord(self.guess[x])
+        self.lowercase = range(97, 122)
+        self.uppercase = range(65, 90)
+
+        if self.order not in self.lowercase or self.uppercase:
+            raise ValueError('Invalid guess')
+
+    def __str__(self):
+        self.guess = self.guess.upper()
+        return f'{self.guess}'
