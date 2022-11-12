5c695f06ebdaa1f7d62cda63f17490135ff7d342
annavmonroy <valades.a@northeastern.edu>
2022-11-12 15:30:45 -0500

    checking guess and english word

--- check_guess.py
+++ check_guess.py
@@ -1,26 +1,51 @@
+import unidecode
+
 # only checks if guess is a valid input
 class Check_Guess:
 
-    def __init_(self, guess, order, lowercase, uppercase):
+    def __init_(self, guess, order, lowercase, uppercase, words):
         self.guess = guess
         self.order = order
         self.lowercase = lowercase
         self.uppercase = uppercase
+        self.words = words
+        self.check = False
 
-    def length(self):   # length = 5
+    def check_length(self):   # length = 5
         if len(self.guess) != 5:
             raise ValueError('Invalid guess')
 
-    def letters(self):  # no symbols, accent marks, tildes, spaces, etc. # convert everything to uppercase
+    def check_letters(self):  # no symbols, accent marks, tildes, spaces, etc. # convert everything to uppercase
         for x in range(0, len(self.guess)):
             # converts the value in the string to it's unicode code 
             self.order = ord(self.guess[x])
-        self.lowercase = range(97, 122)
-        self.uppercase = range(65, 90)
+            self.lowercase = range(97, 122)
+            self.uppercase = range(65, 90)
+        
+            if self.order not in self.lowercase or self.uppercase:
+                raise ValueError('Invalid guess')
+    
+    def check_word_bank(self):
+        # checks if guess is in the english or spanish word bank
+        self.guess = self.guess.lower()
+        # checks english word bank
+        with open('english.txt') as f:
+            self.words = f.read().splitlines()
+            if self.guess == self.words:
+                self.check = True
 
-        if self.order not in self.lowercase or self.uppercase:
-            raise ValueError('Invalid guess')
+        # checks spanish word bank
+        with open('spanish.txt', 'rt', encoding = 'utf-8') as f:
+            words = f.read().splitlines()
+            for i in range(len(words)):
+                words[i] = unidecode.unidecode(words[i])
+            if self.guess == self.words:
+                self.check = True
+        
+        if self.check == False:
+            raise ValueError('Word not in word bank')
 
     def __str__(self):
+        # converts guess to uppercase
         self.guess = self.guess.upper()
         return f'{self.guess}'
--- first_word.py
+++ first_word.py
@@ -1,4 +1,24 @@
-class First_Word:
+# import the random word and the guess input
+# letters for both words are in a list ex: guess = [a, b, c, d, e]?
+class English_Word:
 
-    def __init(self):
+    def __init(self, guess, random):
+        self.guess = guess # replace this with the imported guess input
+        self.random = random # replace this with the imported random word
+        self.green_letters = []
+        self.yellow_letters = []
+
+    def check_green_letters(self):
+        for x in range(0, len(self.guess)):
+            for y in range(0, len(self.random)):
+                if self.guess[x] == self.random[y]:
+                    self.green_letters.append(self.guess[x])
+    
+    def check_yellow_letters(self):
+        for x in range(0, len(self.guess)):
+            if self.guess[x] in self.random and self.guess[x] not in self.green_letters:
+                self.yellow_letters.append(self.guess[x])
+
+# self.guess returned with letters in green, yellow, and white
+    def __str__(self):
         pass
