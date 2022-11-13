c5766bdc37e7cdc3d46685c64ba00caa0dbd7afa
annavmonroy <valades.a@northeastern.edu>
2022-11-13 18:27:10 -0500

    inheritance relationship

--- english.py
+++ english.py
@@ -1,9 +1,17 @@
 import random
 
-def random_word():
-    with open('english.txt') as f:
-        words = f.read().splitlines()
-    word = random.choice(words)
-    return word.upper()
+class Get_Random_English:
 
-print(random_word())
+    def __init__(self, filename):
+        self.file = filename
+
+
+    def random_word(self):
+        with open(self.file) as f:
+            words = f.read().splitlines()
+        word = random.choice(words)
+        return word.upper()
+
+
+first_word = Get_Random_English('english.txt')
+print(first_word.random_word())
--- spanish.py
+++ spanish.py
@@ -1,14 +1,18 @@
 import random
 import unidecode
+from english import Get_Random_English
 
-def random_word():
-    # reads file with accent marks
-    with open('spanish.txt', 'rt', encoding = 'utf-8') as f:
-        words = f.read().splitlines()
-        # removes accent marks and tildes
-        for i in range(len(words)):
-            words[i] = unidecode.unidecode(words[i])
-    word = random.choice(words)
-    return word.upper()
+class Get_Random_Spanish(Get_Random_English):
 
-print(random_word())
+    def random_word(self):
+        # reads file with accent marks
+        with open('spanish.txt', 'rt', encoding = 'utf-8') as f:
+            words = f.read().splitlines()
+            # removes accent marks and tildes
+            for i in range(len(words)):
+                words[i] = unidecode.unidecode(words[i])
+        word = random.choice(words)
+        return word.upper()
+
+second_word = Get_Random_Spanish('english.txt')
+print(second_word.random_word())
