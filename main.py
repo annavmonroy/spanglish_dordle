2cdfe63d7597a45eed47de95332d53cd462918f6
annavmonroy <valades.a@northeastern.edu>
2022-11-22 18:04:58 -0500

    little stuff

--- main.py
+++ main.py
@@ -4,11 +4,9 @@ from dordle import Dordle
 
 first_word = Get_Random_English('english.txt')
 english_random = first_word.random_word()
-print(english_random)
 
 second_word = Get_Random_Spanish('spanish.txt')
 spanish_random = second_word.random_word()
-print(spanish_random)
 
 guess = ''
 word = Dordle(guess, english_random, spanish_random)
